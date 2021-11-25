449b92adb07721723c4deb6294a0ca59f0ff6405
space thyme <52584837+spacethyme@users.noreply.github.com>
2021-11-25 15:29:38 -0800

    Delete keys.js

--- src/keys.js
+++
@@ -1,6 +0,0 @@
-// Adding this file to the git repository as a template with all information blanked out
-// After template is added, this file will be put on the .gitignore list
-// This way other users can fill in the template, but the file will not get updated in the respository
-
-// Discogs "personal access token" goes here:
-export const token = "";
