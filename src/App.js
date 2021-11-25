006a1d16124b61a7aa5fafd498f8888ae84fad80
space thyme <spacethyme@shaw.ca>
2021-11-25 15:05:38 -0800

    get data, dump it into browser

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
+import axios from 'axios';
+import { useState } from 'react';
 import './App.css';
 
+// Discogs issues a temporary "personal access token" which should be kept private
+// "token" is a constant stored in "keys.js", which is not included in git repository.
+import { token } from './keys.js';
+
 export default function App() {
+
+  const [data, setData] = useState([]);
+  const dump = JSON.stringify(data);
+  const [query, setQuery] = useState("sgt pepper")
+
+  const fetchUrl = `https://api.discogs.com/database/search?q=${query}`;
+
+  const fetchImages = () => {
+    axios
+      .get(fetchUrl, {
+        headers: {
+          authorization: `Discogs token=${token}`
+        },
+      })
+      .then((response) => {
+        setData(response.data.results[0]);
+        console.log(response.data.results[0]);
+      })
+      .catch((error) => {
+        setData(error);
+      });
+  }
+
   return (
     <div className="App">
-      <p>foo, bar, &amp; blech to you too</p>
+      <p>Current query term = {query}</p>
+      <input type="text" value={query} onChange={(e) => setQuery(e.target.value)} />
+      <button onClick={fetchImages}>Search</button>
+      <hr />
+      <p>{dump}</p>
     </div>
   );
 }
