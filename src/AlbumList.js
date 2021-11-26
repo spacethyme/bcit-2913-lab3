e9a33c99eddae0e7c2b4f9de1c938e97e618194b
space thyme <spacethyme@shaw.ca>
2021-11-26 01:13:19 -0800

    prettier

--- src/AlbumList.js
+++ src/AlbumList.js
@@ -8,8 +8,8 @@ export default function AlbumList( {data, setActiveIndex, query, setQuery} ) {
 
   return (
     <section className="album-list">
+      <label>Search Albums:</label>
       <input onKeyUp={searchImages} type="text" placeholder={query} />
-      <h2>Results:</h2>
       {data.map((a, key) => (
         <button onClick={() => {setActiveIndex(key)}} key={key}>
           <img src={a.thumb} alt={a.resource_url} />
